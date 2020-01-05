179adf2585431ce9108dec1936b9083123c73c9a
Jerry <jerry@gmail.com>
2020-01-05 23:47:59 +0400

    added vue ui and vuetify

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex>
+        about
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
